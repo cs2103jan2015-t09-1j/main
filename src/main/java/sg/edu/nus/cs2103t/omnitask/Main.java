cc50a4e575e1467208afb0de8e13def61aedb9af
Faruq Rasid <me@ruqqq.sg>
2015-02-06 16:50:57 +0800

    Initial commit. Hello World and GSON as Maven dependency.

---
+++ src/main/java/sg/edu/nus/cs2103t/omnitask/Main.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.cs2103t.omnitask;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}

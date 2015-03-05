8286eed81dc087b40732d7da9dcb2bb0efeea70f
sijie <chensjiesb@gmail.com>
2015-03-05 09:15:33 +0800

    test

--- src/main/java/sg/edu/nus/cs2103t/omnitask/ui/UI.java
+++ src/main/java/sg/edu/nus/cs2103t/omnitask/ui/UI.java
@@ -5,7 +5,7 @@ public abstract class UI {
 	
 	public abstract void showError(String msg);
 	
-	public abstract void start();
+	public abstract void start();//push test sijie
 	
 	public abstract void exit();
 }

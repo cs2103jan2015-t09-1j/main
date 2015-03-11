a441da8e47097902a7c74b2a2a9ea68341df64ea
Faruq Rasid <me@ruqqq.sg>
2015-03-11 11:48:24 +0800

    Implemented Natty to parse date and time.

--- src/main/java/sg/edu/nus/cs2103t/omnitask/parser/ParserMainImpl.java
+++ src/main/java/sg/edu/nus/cs2103t/omnitask/parser/ParserMainImpl.java
@@ -1,54 +1,80 @@
 package sg.edu.nus.cs2103t.omnitask.parser;
 
 import sg.edu.nus.cs2103t.omnitask.model.CommandInput;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
+
+import com.joestelmach.natty.DateGroup;
+
 import java.lang.Object;
+import java.util.List;
+
+import org.antlr.runtime.tree.Tree;
+import org.joda.time.DateTime;
+
 public class ParserMainImpl extends Parser {
 
-private static final String STARTDATE_INDICATOR = "from";
-private static final String ENDDATE_INDICATOR = "to";
+	private static final String[] DATE_INDICATORS = new String[]{"from", "by", "due"};
 
-@Override
+	@Override
 	public CommandInput parseUserInput(String input) {
 		// TODO: Fix prototype implementation, need to think of the proper way
 		// to parse text modularly
 		String[] inputSplit = input.split(" ");
-		org.joda.time.format.DateTimeFormatter fmt = DateTimeFormat.forPattern("MM dd yyyy");
-		String sDbeforeParse = new String();
-        String eDbeforeParse = new String();
-        
+
 		// need to add !inputSplit[0].equals(Commands.COMMAND_DELETE) for
 		// subsequent commands to validate if user input a valid command
 		if (!inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_ADD)
-				&& !inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_DISPLAY)
-				&& !inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_DELETE)
-				&& !inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_EDIT)
-				&& !inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_EXIT)) {
+				&& !inputSplit[0].toLowerCase().equals(
+						CommandInput.COMMAND_DISPLAY)
+				&& !inputSplit[0].toLowerCase().equals(
+						CommandInput.COMMAND_DELETE)
+				&& !inputSplit[0].toLowerCase().equals(
+						CommandInput.COMMAND_EDIT)
+				&& !inputSplit[0].toLowerCase().equals(
+						CommandInput.COMMAND_EXIT)) {
 			return null;
 		}
-
-		CommandInput commandInput = new CommandInput(inputSplit[0].toLowerCase());
-		if (inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_ADD)) {
-			String name = "";
+		
+		CommandInput commandInput = new CommandInput(inputSplit[0]);
+		
+		if (commandInput.getCommandName().equals(CommandInput.COMMAND_ADD)) {
+			String taskName = "";
+			
 			for (int i = 1; i < inputSplit.length; i++) {
-				if(!inputSplit[i].equals(STARTDATE_INDICATOR) 
-						&& !inputSplit[i].equals(ENDDATE_INDICATOR)
-						&& !inputSplit[i-1].equals(STARTDATE_INDICATOR)
-						&& !inputSplit[i-1].equals(ENDDATE_INDICATOR)){//recognize date time in string
-				name += inputSplit[i] + " ";
-				}
-				else if(inputSplit[i-1].equals(STARTDATE_INDICATOR)){
-					sDbeforeParse = inputSplit[i];
-				}
-				else if(inputSplit[i-1].equals(ENDDATE_INDICATOR)){
-					eDbeforeParse = inputSplit[i];
+				if (inArray(DATE_INDICATORS, inputSplit[i])) {
+					taskName = joinStringArray(inputSplit, 1, i);
+					
+					// Parse date using Natty
+					com.joestelmach.natty.Parser parser = new com.joestelmach.natty.Parser();
+					List<DateGroup> groups = parser.parse(input);
+					for (DateGroup group : groups) {
+						// If there are 2 dates, means it's to and from
+						if (group.getDates().size() == 2) {
+							commandInput.setStartDate(new DateTime(group.getDates().get(0).getTime()));
+							if (!isTimeSpecifiedByUser(group.getSyntaxTree().getChild(0))) {
+								commandInput.setStartDate(commandInput.getStartDate().withMillisOfDay(0));
+							}
+							
+							commandInput.setEndDate(new DateTime(group.getDates().get(1).getTime()));
+							if (!isTimeSpecifiedByUser(group.getSyntaxTree().getChild(1))) {
+								commandInput.setEndDate(commandInput.getEndDate().withMillisOfDay(0));
+							}
+						} else {
+							commandInput.setEndDate(new DateTime(group.getDates().get(0).getTime()));
+							if (!isTimeSpecifiedByUser(group.getSyntaxTree().getChild(0))) {
+								commandInput.setEndDate(commandInput.getEndDate().withMillisOfDay(0));
+							}
+						}
+					}
+					
+					break;
 				}
 			}
-			commandInput.setName(name.trim());
-			commandInput.setStartDate(DateTime.now());
-			//commandInput.setStartDate(DateTime.parse(sDbeforeParse, fmt));
-			//commandInput.setEndDate(DateTime.parse(eDbeforeParse, fmt));
+			
+			if (taskName.equals("")) {
+				taskName = joinStringArray(inputSplit, 1, inputSplit.length);
+			}
+			
+			commandInput.setName(taskName.trim());
 		}
 
 		// this is to parse command specific to delete
@@ -71,12 +97,44 @@ private static final String ENDDATE_INDICATOR = "to";
 			}
 			commandInput.setName(name.trim());
 		}
-		
+
 		if (inputSplit[0].toLowerCase().equals(CommandInput.COMMAND_EXIT)) {
 			commandInput.setName("exit");
 		}
 
 		return commandInput;
 	}
-
+	
+	// Do case-insensitive search of word in array
+	private boolean inArray(String[] haystack, String needle) {
+		for (int i = 0; i < haystack.length; i++) {
+			if (haystack[i].toLowerCase().equals(needle.toLowerCase())) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	// Join a string array with a space
+	private String joinStringArray(String[] strArray, int start, int end) {
+		String str = "";
+		for (int i = start; i < end; i++) {
+			str += strArray[i] + " ";
+		}
+		
+		return str.trim();
+	}
+	
+	private boolean isTimeSpecifiedByUser(Tree tree) {
+		boolean timeSpecified = false;
+		for (int j = 0; j < tree.getChildCount(); j++) {
+			if (tree.getChild(j).getText().equals("EXPLICIT_TIME")) {
+				timeSpecified = true;
+				break;
+			}
+		}
+		
+		return timeSpecified;
+	}
 }

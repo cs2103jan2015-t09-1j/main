7db18f8a6944c83068deb66c7d76c73ba754a6c9
Faruq Rasid <me@ruqqq.sg>
2015-03-18 22:23:57 +0800

    Implemented basic custom logging to file (log.txt).

---
+++ src/main/java/sg/edu/nus/cs2103t/omnitask/Logger.java
@@ -0,0 +1,38 @@
+package sg.edu.nus.cs2103t.omnitask;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import org.joda.time.DateTime;
+
+public class Logger {
+	private static Path logFile = new File("log.txt").toPath();
+	
+	private static void writeToFile(Path file, String string) {
+		try {
+			if (!file.toFile().exists()) {
+				Files.createFile(file);
+			}
+			
+			OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, StandardOpenOption.WRITE, StandardOpenOption.APPEND));
+			out.write((string).getBytes());
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void writeDebug(String msg) {
+		writeToFile(logFile, DateTime.now() + " [D]: " + msg + "\r\n");
+	}
+	
+	public static void writeError(String msg) {
+		writeToFile(logFile, DateTime.now() + " [E]: " + msg + "\r\n");
+	}
+}

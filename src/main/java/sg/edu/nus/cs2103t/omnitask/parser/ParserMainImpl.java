aa9a76a005156d5f30bdb8630ece7eaffb5eba56
Sim Wei Kang <wksim91@yahoo.com.sg>
2015-03-23 19:59:19 +0800

    Solved EditTask Bug.
    Now able to change Task's Name and/or Date

--- src/main/java/sg/edu/nus/cs2103t/omnitask/parser/ParserMainImpl.java
+++ src/main/java/sg/edu/nus/cs2103t/omnitask/parser/ParserMainImpl.java
@@ -124,41 +124,52 @@ public class ParserMainImpl extends Parser {
 			updateId = Long.parseLong(inputSplit[1]);
 			commandInput.setId(updateId);
 						
-			for (int i = 2; i < inputSplit.length; i++) {
+			for (int i = 1; i < inputSplit.length; i++) {
 				if (inArray(DATE_INDICATORS, inputSplit[i])) {
 					taskName = joinStringArray(inputSplit, 2, i);
-					
+
 					// Parse date using Natty
 					com.joestelmach.natty.Parser parser = new com.joestelmach.natty.Parser();
 					List<DateGroup> groups = parser.parse(input);
 					for (DateGroup group : groups) {
 						// If there are 2 dates, means it's to and from
-						// If no specific time is specified by user, set the time to 00:00:00, retaining the dates
+						// If no specific time is specified by user, set the
+						// time to 00:00:00, retaining the dates
 						if (group.getDates().size() == 2) {
-							commandInput.setStartDate(new DateTime(group.getDates().get(0).getTime()));
-							if (!isTimeSpecifiedByUser(group.getSyntaxTree().getChild(0))) {
-								commandInput.setStartDate(commandInput.getStartDate().withMillisOfDay(0));
+							commandInput.setStartDate(new DateTime(group
+									.getDates().get(0).getTime()));
+							if (!isTimeSpecifiedByUser(group.getSyntaxTree()
+									.getChild(0))) {
+								commandInput.setStartDate(commandInput
+										.getStartDate().withMillisOfDay(0));
 							}
-							
-							commandInput.setEndDate(new DateTime(group.getDates().get(1).getTime()));
-							if (!isTimeSpecifiedByUser(group.getSyntaxTree().getChild(1))) {
-								commandInput.setEndDate(commandInput.getEndDate().withMillisOfDay(0));
+
+							commandInput.setEndDate(new DateTime(group
+									.getDates().get(1).getTime()));
+							if (!isTimeSpecifiedByUser(group.getSyntaxTree()
+									.getChild(1))) {
+								commandInput.setEndDate(commandInput
+										.getEndDate().withMillisOfDay(0));
 							}
 						} else {
-							commandInput.setEndDate(new DateTime(group.getDates().get(0).getTime()));
-							if (!isTimeSpecifiedByUser(group.getSyntaxTree().getChild(0))) {
-								commandInput.setEndDate(commandInput.getEndDate().withMillisOfDay(0));
+							commandInput.setEndDate(new DateTime(group
+									.getDates().get(0).getTime()));
+							if (!isTimeSpecifiedByUser(group.getSyntaxTree()
+									.getChild(0))) {
+								commandInput.setEndDate(commandInput
+										.getEndDate().withMillisOfDay(0));
 							}
 						}
 					}
-					
+
 					break;
 				}
+
+				if (taskName.equals("")) {
+					taskName = joinStringArray(inputSplit, 2, inputSplit.length);
+				}
 			}
-					
-//			if (taskName.equals("")) {
-//				taskName = joinStringArray(inputSplit, 2, inputSplit.length);
-//			}
+
 			
 			commandInput.setName(taskName.trim());
 			

cad8e2556d2122a635d1009bca3e1fd52e1a9b59
Sim Wei Kang <wksim91@yahoo.com.sg>
2015-03-25 23:15:13 +0800

    WK's jUnit for Add (not working)

--- src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitDataAtd.java
+++ src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitDataAtd.java
@@ -1,16 +1,24 @@
 package sg.edu.nus.cs2103t.omnitask;
 
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
 import org.junit.Test;
 
+import sg.edu.nus.cs2103t.omnitask.logic.Data.TaskNoNameException;
 import sg.edu.nus.cs2103t.omnitask.logic.DataImpl;
+import sg.edu.nus.cs2103t.omnitask.model.CommandInput;
+import sg.edu.nus.cs2103t.omnitask.model.Task;
 import sg.edu.nus.cs2103t.omnitask.storage.IOStubImpl;
 
 public class JUnitDataAtd {
 	
 	@Test
-	public void TestAdd() {
+	public void TestAdd() throws TaskNoNameException, IOException {
 		DataImpl data = DataImpl.GetSingleton();
 		
 		// Needed because of method signature
@@ -20,8 +28,15 @@ public class JUnitDataAtd {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		// Craft tests below
+		addOneTask(data, "Testing");
+		
 	}
-	
+	private void addOneTask(final DataImpl data, final String name) throws TaskNoNameException, IOException {
+		Task task = new Task();
+		task.setName(name);
+		
+		data.addTask(task);
+		assertEquals(data.getTasks().get(0).getId(), task.getId());
+	}
 }

50a273d0edb5e4ad4d5b40daa39edf346019e221
Faruq Rasid <me@ruqqq.sg>
2015-03-25 21:46:20 +0800

    Removed unneeded imports in JUnitDataAtd.

--- src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitDataAtd.java
+++ src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitDataAtd.java
@@ -1,20 +1,11 @@
 package sg.edu.nus.cs2103t.omnitask;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.io.IOException;
 
-import org.joda.time.DateTime;
 import org.junit.Test;
 
-import sg.edu.nus.cs2103t.omnitask.logic.Data;
 import sg.edu.nus.cs2103t.omnitask.logic.DataImpl;
-import sg.edu.nus.cs2103t.omnitask.model.CommandInput.CommandType;
-import sg.edu.nus.cs2103t.omnitask.parser.Parser;
-import sg.edu.nus.cs2103t.omnitask.parser.ParserMainImpl;
 import sg.edu.nus.cs2103t.omnitask.storage.IOStubImpl;
-import sg.edu.nus.cs2103t.omnitasks.command.Command;
 
 public class JUnitDataAtd {
 	

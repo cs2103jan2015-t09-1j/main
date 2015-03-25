42bee2e5ced4dd7c3f5c2f241482f16fe6a27eb3
Sim Wei Kang <wksim91@yahoo.com.sg>
2015-03-25 23:56:32 +0800

    WK's Data jUnit (Rough)
    Current build only able to test for:
    Adding of 1 task...

--- src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitDataAtd.java
+++ src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitDataAtd.java
@@ -4,14 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import org.joda.time.DateTime;
 import org.junit.Test;
 
 import sg.edu.nus.cs2103t.omnitask.logic.Data.TaskNoNameException;
 import sg.edu.nus.cs2103t.omnitask.logic.DataImpl;
-import sg.edu.nus.cs2103t.omnitask.model.CommandInput;
 import sg.edu.nus.cs2103t.omnitask.model.Task;
 import sg.edu.nus.cs2103t.omnitask.storage.IOStubImpl;
 
@@ -35,8 +32,34 @@ public class JUnitDataAtd {
 	private void addOneTask(final DataImpl data, final String name) throws TaskNoNameException, IOException {
 		Task task = new Task();
 		task.setName(name);
-		
+		assertEquals(true, data.getTasks().isEmpty());
 		data.addTask(task);
-		assertEquals(data.getTasks().get(0).getId(), task.getId());
+		//test if the task is added into the array list.
+		assertEquals(task.getId(), data.getTasks().get(0).getId());
 	}
+//	@Test
+//	public void TestDelete() throws TaskNoNameException, IOException  {
+//		DataImpl data = DataImpl.GetSingleton();
+//		
+//		// Needed because of method signature
+//		// Technically exception would never ever be throw if using IOStubImpl
+//		try {
+//			data.init(new IOStubImpl());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		// Craft tests below
+//		deleteOneTask(data, "deleteTest");
+//		
+//	}
+//	private void deleteOneTask(final DataImpl data, final String name) throws TaskNoNameException, IOException {
+//		Task task = new Task();
+//		task.setName(name);
+//		
+//		data.addTask(task);
+//		//assertEquals(data.getTasks().get(0).getId(), task.getId());
+//		data.deleteTask(task);
+//		assertEquals(true, data.getTasks().isEmpty());
+//		
+//	}
 }

d85b5912c67fbc48595a59a7cd319c165882a809
sijie <chensjiesb@gmail.com>
2015-03-25 22:19:11 +0800

    added junit test cases

--- src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitParserAtd.java
+++ src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitParserAtd.java
@@ -22,8 +22,33 @@ public class JUnitParserAtd {
 		assertEquals(command.getCommandInput().getCommandType(), CommandType.ADD);
 		assertEquals(command.getCommandInput().getName(), "Hello");
 		
+		//Test parsing of "new" and "task name"
+		command = parseUserInputHelper("new Hello");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.ADD);
+		assertEquals(command.getCommandInput().getName(), "Hello");
+		
+		
 		// Test parsing of "add" and "task name", "due" parameter
 		command = parseUserInputHelper("add Hello due 15 August 2015");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.ADD);
+		assertEquals(command.getCommandInput().getName(), "Hello");
+		assertEquals(command.getCommandInput().getEndDate().dayOfMonth().get(), 15);
+		assertEquals(command.getCommandInput().getEndDate().monthOfYear().get(), 8);
+		assertEquals(command.getCommandInput().getEndDate().year().get(), 2015);
+		
+		//Test parsing of "add" and "task name", "by" parameter
+		command = parseUserInputHelper("add Hello by 15 August 2015");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.ADD);
+		assertEquals(command.getCommandInput().getName(), "Hello");
+		assertEquals(command.getCommandInput().getEndDate().dayOfMonth().get(), 15);
+		assertEquals(command.getCommandInput().getEndDate().monthOfYear().get(), 8);
+		assertEquals(command.getCommandInput().getEndDate().year().get(), 2015);
+		
+		//Test parsing of "add", "task name" and "on" parameter
+		command = parseUserInputHelper("add Hello on 15 August 2015");
 		assertNotNull(command);
 		assertEquals(command.getCommandInput().getCommandType(), CommandType.ADD);
 		assertEquals(command.getCommandInput().getName(), "Hello");
@@ -37,6 +62,44 @@ public class JUnitParserAtd {
 		assertEquals(command.getCommandInput().getCommandType(), CommandType.ADD);
 		assertEquals(command.getCommandInput().getName(), "Hello");
 		assertEquals(command.getCommandInput().getEndDate().dayOfYear().get(), DateTime.now().plusDays(1).dayOfYear().get());
+	
+	    //Test parsing of "delete" and "task index" parameter
+		command = parseUserInputHelper("delete 1");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.DELETE);
+		assertEquals(command.getCommandInput().getId(), 1);
+		
+		//Test parsing of "display" parameter
+		command = parseUserInputHelper("display");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.DISPLAY);
+		
+		//Test parsing of "edit" and "task index" and "task name" parameter
+		command = parseUserInputHelper("edit 1 Hello");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.EDIT);
+		assertEquals(command.getCommandInput().getId(), 1);
+		assertEquals(command.getCommandInput().getName(), "Hello");
+		
+		//Test parsing of "edit", "task index" and "due" parameter
+		command = parseUserInputHelper("edit 1 due 15 August 2015");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.EDIT);
+		assertEquals(command.getCommandInput().getId(), 1);
+		assertEquals(command.getCommandInput().getEndDate().dayOfMonth().get(), 15);
+		assertEquals(command.getCommandInput().getEndDate().monthOfYear().get(), 8);
+		assertEquals(command.getCommandInput().getEndDate().year().get(), 2015);
+		
+		//Test parsing of "exit" parameter
+		command = parseUserInputHelper("exit");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.EXIT);
+		
+		//Test parsing of "search" and "search content"
+		command = parseUserInputHelper("search Hello");
+		assertNotNull(command);
+		assertEquals(command.getCommandInput().getCommandType(), CommandType.SEARCH);
+		assertEquals(command.getCommandInput().getName(), "Hello");
 	}
 	
 	private Command parseUserInputHelper(String input) {

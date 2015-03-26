3850b59f52863e6f226c52eae9c82b6cb56dd47f
Faruq Rasid <me@ruqqq.sg>
2015-03-26 08:50:47 +0800

    Added a few more test cases and comments to Commands JUnit.

--- src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitCommandsAtd.java
+++ src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitCommandsAtd.java
@@ -17,16 +17,32 @@ import sg.edu.nus.cs2103t.omnitasks.command.CommandEditImpl;
 
 public class JUnitCommandsAtd {
 	
+	// Test add command, make sure it is added successfully
 	@Test
 	public void TestCommandAdd() {
 		DataStubImpl data = initData();
-		addOneTask(data, "Hello Add");
+		addOneTaskShouldPass(data, "Hello Add");
 	}
 	
+	// Test add command, make sure empty string will not add successfully
+	@Test
+	public void TestCommandAddFail1() {
+		DataStubImpl data = initData();
+		addOneTaskShouldFail(data, "");
+	}
+	
+	// Test add command, make sure empty (untrimmed) string will not add successfully
+	@Test
+	public void TestCommandAddFail2() {
+		DataStubImpl data = initData();
+		addOneTaskShouldFail(data, " ");
+	}
+	
+	// Test edit command, make sure edited tasks are saved properly
 	@Test
 	public void TestCommandEdit() {
 		final DataStubImpl data = initData();
-		addOneTask(data, "Hello Edit");
+		addOneTaskShouldPass(data, "Hello Edit");
 		
 		CommandInput commandInput = new CommandInput(CommandType.EDIT);
 		commandInput.setId(1);
@@ -62,7 +78,8 @@ public class JUnitCommandsAtd {
 		return data;
 	}
 	
-	private void addOneTask(final DataStubImpl data, final String name) {
+	// Assert if fail
+	private void addOneTaskShouldPass(final DataStubImpl data, final String name) {
 		CommandInput commandInput = new CommandInput(CommandType.ADD);
 		commandInput.setName(name);
 		
@@ -77,6 +94,25 @@ public class JUnitCommandsAtd {
 
 			public void onFailure(String msg) {
 				throw new AssertionError("Command should not fail at all!");
+			}
+			
+		});
+	}
+	
+	// Assert if pass
+	private void addOneTaskShouldFail(final DataStubImpl data, final String name) {
+		CommandInput commandInput = new CommandInput(CommandType.ADD);
+		commandInput.setName(name);
+		
+		Command command = new CommandAddImpl(commandInput);
+		command.processCommand(data, new CommandResultListener() {
+
+			public void onSuccess(String msg) {
+				throw new AssertionError("Command should not pass at all!");
+			}
+
+			public void onFailure(String msg) {
+				
 			}
 			
 		});

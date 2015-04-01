9dc2457e6e9312712050bda14c42c0ba74beb3ca
Faruq Rasid <me@ruqqq.sg>
2015-04-01 15:33:24 +0800

    Renamed arguments in SystemTest JUnit for readability.

--- src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitSystemTest.java
+++ src/test/java/sg/edu/nus/cs2103t/omnitask/JUnitSystemTest.java
@@ -39,18 +39,20 @@ public class JUnitSystemTest {
 		processInput(ui, parser, data, "add Hello World Tmr due tomorrow", CommandType.ADD, 0, "Hello World Tmr", null, new DateTime().withMillisOfDay(0).plusDays(1), Priority.NONE);
 		processInput(ui, parser, data, "delete 1", CommandType.DELETE, 1, null, null, null, Priority.NONE);
 		processInput(ui, parser, data, "edit 1 Hello World Edited", CommandType.EDIT, 1, "Hello World Edited", null, null, Priority.NONE);
+	
+		// TODO: Check for results in data class
 	}
 	
-	private void processInput(UI ui, Parser parser, Data data, String input, CommandType commandType, long id, String taskName, DateTime startDate, DateTime endDate, Priority priority) {
+	private void processInput(UI ui, Parser parser, Data data, String input, CommandType expectedCommandType, long expectedId, String expectedTaskName, DateTime expectedStartDate, DateTime expectedEndDate, Priority expectedPriority) {
 		Command command = parser.parseUserInput(input);
 
 		assertNotNull(command);
-		assertEquals(commandType, command.getCommandInput().getCommandType());
-		assertEquals(id, command.getCommandInput().getId());
-		assertEquals(taskName, command.getCommandInput().getName());
-		assertEquals(startDate, command.getCommandInput().getStartDate());
-		assertEquals(endDate, command.getCommandInput().getEndDate());
-		assertEquals(priority, command.getCommandInput().getPriority());
+		assertEquals(expectedCommandType, command.getCommandInput().getCommandType());
+		assertEquals(expectedId, command.getCommandInput().getId());
+		assertEquals(expectedTaskName, command.getCommandInput().getName());
+		assertEquals(expectedStartDate, command.getCommandInput().getStartDate());
+		assertEquals(expectedEndDate, command.getCommandInput().getEndDate());
+		assertEquals(expectedPriority, command.getCommandInput().getPriority());
 		
 		boolean success = command.processCommand(data, ui);
 		assertTrue(success);
